3174e47a7379b6d5ba79731ede239d95778e0002
KeriaDaring <Keria@973289634@qq.com>
2023-11-10 19:10:12 +0800

    experiment7 done

--- 12223020208-龙万军/experiment/experiment6/stage2/task3/script/main.js
+++ 12223020208-龙万军/experiment/experiment6/stage2/task3/script/main.js
@@ -2,23 +2,41 @@
 
 
 
-let color_list = ['#999999', '#0033ff', '#ffff67', '#ffff00', '#6600ce', '#0099ff', '#996533', '#ff6501', '#00cd34' ];
 
-for (let i = 1; i <= 9; i++) {
-    const elem = document.querySelector('#sq_' + i);
-    elem.style.backgroundColor = color_list[i - 1];
-}
+let selected_target;
+let className;
 
-document.querySelector('#square').addEventListener('click', (event) => {
-    let target = event.target.closest('td');
-    if (target) {
-        target.style.backgroundColor = 'red';
-        let list = target.querySelectorAll('div');
+const table = document.getElementById('square');
 
-        for (let i in list) {
-            list[i].style.color = 'white';
-        }
+
+table.onclick = function(event) {
+    let td = event.target.closest('td');
+
+    if (!td) return;
+    if (!table.contains(td)) return;
+
+
+    highlight(td);
+};
+
+
+function highlight(target) {
+
+    if (selected_target)  {
+        selected_target.classList.remove('highlight');
     }
     
-});
+    selected_target = target;
+    target.classList.add('highlight');
+
+}
+
+
+
+// let color_list = ['#999999', '#0033ff', '#ffff67', '#ffff00', '#6600ce', '#0099ff', '#996533', '#ff6501', '#00cd34' ];
+
+// for (let i = 1; i <= 9; i++) {
+//     const elem = document.querySelector('#sq_' + i);
+//     elem.style.backgroundColor = color_list[i - 1];
+// }
 
---
+++ 12223020208-龙万军/experiment/experiment7/stage1/task2/script/main.js
@@ -0,0 +1,9 @@
+let price = "235.4"
+let num = "5"
+
+let sum;
+
+sum = parseFloat(price) * parseInt(num);
+
+window.alert("您将购买的商品的单价为“" + price + "元“" + "您将有购买“" +
+            num + "个“,金额为“" + sum + "元”");
